a01358f19120e1464e0887028479f88deb78deb5
Aaron Elizondo <acelizondo11@gmail.com>
2021-09-06 12:47:34 -0500

    Updating controller actions for form submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
 
-    def create 
-
+    def new
+        @user = User.new
     end
-    
+
+    def create 
+        @user = User.new(username: params[:username], password: params[:password], email: params[:email])
+
+        if @user.save
+            redirect_to new_user_path
+        else
+            render :new
+        end
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true
-    validates :password, presence: true
+    validates :password, presence: true, length: {minimum: 3}
     validates :email, presence: true
 
 end
